de7c95c2b92f3be5dfd3842b636646686a6c7362
Rasika Thete <148611013+Rasikathete@users.noreply.github.com>
2024-02-06 14:07:11 +0530

    Update and rename add.c to division.c

---
+++ division.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num, num1, sum;
+    
+    printf("Enter the first number: ");
+    scanf("%d", &num);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num1);
+    
+    sum = num/num1;
+    
+    printf("The division of %d and %d is %d.", num1, num2, sum);
+    
+    return 0;
+}
